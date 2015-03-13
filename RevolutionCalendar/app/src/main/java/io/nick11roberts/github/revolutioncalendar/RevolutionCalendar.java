016c19b8ff3d3cc3706cfb97198a08fd226f48ca
nick11roberts <nick.roberts.127.0.0.1@gmail.com>
2015-03-13 00:10:26 -0700

    Added some date stuff.

--- RevolutionCalendar/app/src/main/java/io/nick11roberts/github/revolutioncalendar/RevolutionCalendar.java
+++ RevolutionCalendar/app/src/main/java/io/nick11roberts/github/revolutioncalendar/RevolutionCalendar.java
@@ -1,20 +1,33 @@
 package io.nick11roberts.github.revolutioncalendar;
 
+import android.app.ActionBar;
+import android.graphics.Point;
 import android.graphics.Typeface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.text.Spannable;
 import android.text.SpannableString;
+import android.util.AttributeSet;
+import android.view.Display;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.software.shell.fab.ActionButton;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 
 public class RevolutionCalendar extends ActionBarActivity {
 
     private Toolbar toolbar;
+    private TextView displayDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +37,39 @@ public class RevolutionCalendar extends ActionBarActivity {
         ActionButton actionButton = (ActionButton) findViewById(R.id.action_button);
         actionButton.playShowAnimation();
 
-        Typeface font = Typeface.createFromAsset(getAssets(),"fonts/LeagueScript.otf");
+        Calendar calendar = Calendar.getInstance();
+
+        Typeface fontToolbar = Typeface.createFromAsset(getAssets(),"fonts/LeagueScript.otf");
         SpannableString s = new SpannableString(getResources().getString(R.string.app_name));
-        s.setSpan(new CustomTypefaceSpan("LeagueScript.otf", font), 0, s.length(),
+        s.setSpan(new CustomTypefaceSpan("LeagueScript.otf", fontToolbar), 0, s.length(),
                 Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
 
+        Typeface fontTextView = Typeface.createFromAsset(getAssets(),"fonts/Roboto-Thin.ttf");
+        SpannableString sDisplayDate = new SpannableString(String.format("%1$tb  %1$td  %1$tY", calendar));
+        sDisplayDate.setSpan(new CustomTypefaceSpan("Roboto-Thin.ttf", fontTextView ), 0, sDisplayDate.length(),
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int width = size.x;
+        int height = size.y;
+
+        displayDate = new TextView(this);
+        displayDate.setTextColor(getResources().getColor(R.color.icons));
+        displayDate.setTextSize(1, 36);
+        displayDate.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.FILL_PARENT);
+        params.weight = 1.0f;
+        params.gravity=17;
+        displayDate.setText(sDisplayDate);
+        displayDate.setLayoutParams(params);
+
         toolbar = (Toolbar) findViewById(R.id.revolutionary_toolbar);
+        toolbar.setMinimumHeight((int)(height/2.61803398875));
+        toolbar.addView(displayDate);
         toolbar.setTitle(s);
-        setSupportActionBar(toolbar);
     }
 
 
