dfa4cd9565b3d73f9e3d93d48403649db3608034
nick11roberts <nick.roberts.127.0.0.1@gmail.com>
2015-03-18 06:23:12 -0700

    Modifications to the toolbar.

--- RevolutionCalendar/app/src/main/java/io/nick11roberts/github/revolutioncalendar/RevolutionCalendar.java
+++ RevolutionCalendar/app/src/main/java/io/nick11roberts/github/revolutioncalendar/RevolutionCalendar.java
@@ -1,26 +1,19 @@
 package io.nick11roberts.github.revolutioncalendar;
 
-import android.app.ActionBar;
 import android.graphics.Point;
 import android.graphics.Typeface;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Spannable;
 import android.text.SpannableString;
-import android.util.AttributeSet;
 import android.view.Display;
-import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.software.shell.fab.ActionButton;
 
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 
@@ -28,6 +21,7 @@ public class RevolutionCalendar extends ActionBarActivity {
 
     private Toolbar toolbar;
     private TextView displayDate;
+    private TextView displayTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,21 +49,23 @@ public class RevolutionCalendar extends ActionBarActivity {
         int width = size.x;
         int height = size.y;
 
-        displayDate = new TextView(this);
-        displayDate.setTextColor(getResources().getColor(R.color.icons));
-        displayDate.setTextSize(1, 36);
-        displayDate.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.FILL_PARENT);
-        params.weight = 1.0f;
-        params.gravity=17;
-        displayDate.setText(sDisplayDate);
-        displayDate.setLayoutParams(params);
+
+
 
         toolbar = (Toolbar) findViewById(R.id.revolutionary_toolbar);
+
+        displayTitle = (TextView) findViewById(R.id.title_text_view);
+        displayTitle.setTextColor(getResources().getColor(R.color.icons));
+        displayTitle.setTextSize(1, 24);
+        displayTitle.setText(s);
+
+        displayDate = (TextView) findViewById(R.id.date_text_view);
+        displayDate.setTextColor(getResources().getColor(R.color.icons));
+        displayDate.setTextSize(1, 36);
+        displayDate.setText(sDisplayDate);
+
         toolbar.setMinimumHeight((int)(height/2.61803398875));
-        toolbar.addView(displayDate);
-        toolbar.setTitle(s);
+
     }
 
 
